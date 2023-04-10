7fba53c9d9f4dfd540ee2b0579b90e40d6c6a3a1
Chad Linthicum <portableanswers@gmail.com>
2023-04-10 02:27:53 -0400

    Converted and published.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,6 +13,7 @@ public class MainMenu : MonoBehaviour
 
     public void Start()
     {
+        
         playerAudio = GetComponent<AudioSource>();
         playerAudio.PlayOneShot(Background, 0.8f);
     }
@@ -20,11 +21,4 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    public void PlayGame2()
-    {
-        Thread.Sleep(5000);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
 }
