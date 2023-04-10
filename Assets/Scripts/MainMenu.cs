1e3773d66f2a6e88e84761951ca604f5f48be85f
Chad Linthicum <portableanswers@gmail.com>
2023-04-09 21:55:12 -0400

    Good measure

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Threading;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGame2()
+    {
+        Thread.Sleep(5000);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
