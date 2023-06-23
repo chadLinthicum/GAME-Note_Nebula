92d25963e91c4f37edcf7f2cd5117a8b7d461134
Chad Linthicum <portableanswers@gmail.com>
2023-06-23 16:32:32 -0400

    Added a door to the level that open upons WIN. Door has no functionality yet.

--- Assets/Scripts/CollectItems.cs
+++ Assets/Scripts/CollectItems.cs
@@ -10,27 +10,45 @@ public class CollectItems : MonoBehaviour
     private AudioSource playerAudio;
     public List<string> Song;
 
+    public void OpenDoors(string tagName, float speed)
+    {
+        // Find the object with the specified tag
+        GameObject obj = GameObject.FindGameObjectWithTag(tagName);
+
+        if (obj == null)
+        {
+            Debug.LogError("Object with tag " + tagName + " not found.");
+            return;
+        }
+
+        // Set the distance to move the object
+        float distance = speed * Time.deltaTime;
+
+        // Move the object by the specified distance
+        obj.transform.position -= new Vector3(distance, 0f, 0f);
+    }
+
     private void Start()
     {
         playerAudio = GetComponent<AudioSource>();
         Song.Add("C");
-        Song.Add("D#");
-        Song.Add("F");
-        Song.Add("C");
-        Song.Add("D#");
-        Song.Add("F#");
-        Song.Add("F");
-        Song.Add("C");
-        Song.Add("D#");
-        Song.Add("F");
-        Song.Add("D#");
-        Song.Add("C");
-        playerAudio.PlayOneShot(Piano_SmokeOnTheWater, 0.3f);
+        //Song.Add("D#");
+        //Song.Add("F");
+        //Song.Add("C");
+        //Song.Add("D#");
+        //Song.Add("F#");
+        //Song.Add("F");
+        //Song.Add("C");
+        //Song.Add("D#");
+        //Song.Add("F");
+        //Song.Add("D#");
+        //Song.Add("C");
+        //playerAudio.PlayOneShot(Piano_SmokeOnTheWater, 0.3f);
     }
-
 
     void OnTriggerEnter(Collider other)
     {
+
         if (Song.Count > 0)
         {
             if (other.gameObject.tag == Song[0])
@@ -38,7 +56,9 @@ public class CollectItems : MonoBehaviour
                 Song.Remove(Song[0]);
                 if (Song.Count == 0)
                 {
-                    playerAudio.PlayOneShot(WIN, 0.6f);
+                    //playerAudio.PlayOneShot(WIN, 0.6f);
+                    OpenDoors("Door_Left", 150f);
+                    OpenDoors("Door_Right", -150f);
                 }
             }
             else if ((other.gameObject.tag == "Button-Play") || (other.gameObject.tag == "Practice_C") || (other.gameObject.tag == "Practice_C#") || (other.gameObject.tag == "Practice_D") || (other.gameObject.tag == "Practice_D#") || (other.gameObject.tag == "Practice_E") || (other.gameObject.tag == "Practice_F") || (other.gameObject.tag == "Practice_F#") || (other.gameObject.tag == "Practice_G") || (other.gameObject.tag == "Practice_G#") || (other.gameObject.tag == "Practice_A") || (other.gameObject.tag == "Practice_A#") || (other.gameObject.tag == "Practice_B"))
